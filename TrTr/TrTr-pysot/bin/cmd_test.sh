2983d40edb12bd0d4fae6ded794f3b62e3818704
qcrs <89289383+qcrs@users.noreply.github.com>
2025-11-18 15:49:16 +0800

    Delete TrTr directory

--- TrTr/TrTr-pysot/bin/cmd_test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-START=26
-END=50
-seq $START 1 $END | \
-    xargs -I {} echo "./models/snapshot/checkpoint_e{}.pth" |  \
-    xargs -I {} python  -u ./bin/my_test.py --snapshot {}  --dataset VOT2018 

eec717e272917643546a350e333a1d24675dc5ea
qcrs <89289383+qcrs@users.noreply.github.com>
2025-11-18 15:48:23 +0800

    Delete SiamFC directory

--- SiamFC/README.md
+++
@@ -1,1 +0,0 @@
-[哔哩哔哩](https://www.bilibili.com/video/BV1GV41117J4)
--- SiamFC/SiamFC/bin/cmd_test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-START=35
-END=49
-seq $START 1 $END | \
-    xargs -I {} echo "./models/siamfc_{}.pth" | \
-    xargs -I {} python -u ./bin/my_test.py    --model_path {}   
